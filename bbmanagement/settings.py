56ac53f51cd20afb1f19c485f938bc3fcd921fc2
Sachin Desai <sachin.desai1399@gmail.com>
2021-11-19 00:11:19 +0530

    Update for Heroku app

--- bbmanagement/settings.py
+++ bbmanagement/settings.py
@@ -128,3 +128,5 @@ STATIC_ROOT = '/static-files/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
